af8fdb377ef062df7474e7a6a586093c623aaf23
Mijan2001 <mijan.cse19@gmail.com>
2024-09-05 15:57:57 +0600

    updated

--- simple stop watch/index.js
+++ simple stop watch/index.js
@@ -3,21 +3,43 @@ const stopEl = document.querySelector('.stop');
 const resetEl = document.querySelector('.reset');
 
 const visibleTimeEl = document.querySelector('.visibleTime');
-
 const currTime = Date.now();
 let updateValue = 1;
 let clearInterValId;
+let elapsedTime = 0;
+let startTime = 0;
+
+const timeUpdate = elapsedTime => {
+    let miliseconds = Math.floor((elapsedTime % 1000) / 10);
+    let seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    let minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+
+    return (
+        (hours ? (hours > 9 ? hours : '0' + hours) : '00') +
+        ':' +
+        (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') +
+        ':' +
+        (seconds ? (seconds > 9 ? seconds : '0' + seconds) : '00') +
+        ':' +
+        (miliseconds > 9 ? miliseconds : '0' + miliseconds)
+    );
+};
 
 startEl.addEventListener('click', () => {
+    startTime = Date.now() - elapsedTime;
+
     clearInterValId = setInterval(() => {
-        visibleTimeEl.innerText = updateValue++;
-    }, 16.67);
+        elapsedTime = Date.now() - startTime;
+        visibleTimeEl.innerText = timeUpdate(elapsedTime);
+    }, 10);
+    console.log(elapsedTime);
 });
 
 stopEl.addEventListener('click', () => {
     clearInterval(clearInterValId);
-    visibleTimeEl.innerText = updateValue;
-    console.log(clearInterValId, updateValue);
+    // visibleTimeEl.innerText = updateValue;
+    // console.log(clearInterValId, updateValue);
 });
 resetEl.addEventListener('click', () => {
     clearInterval(clearInterValId);
---
+++ simple stop watch/practice.js
@@ -0,0 +1,5 @@
+let startTime = 0;
+let elapsedTime = 0;
+
+startTime = Date.now() - elapsedTime;
+console.log(startTime);
